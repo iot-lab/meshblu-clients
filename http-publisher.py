ac09bd369c4d032a55b16dac9be14f0131486bbb
Francisco Cardoso <franciscotcardoso@hotmail.com>
2016-12-27 16:34:04 +0000

    Added http-publisher code initial commit

---
+++ http-publisher.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+from constants import HOST, DEVICE, GATEWAY
+
+def main():
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    body = {
+            'devices': GATEWAY['uuid'],
+            'payload': {
+                        "example_key":"example_value"
+            }
+    }
+
+    print "Publishing to gateway (uuid): {}\n".format(GATEWAY['uuid'])
+    print "Publishing from device (uuid): {}\n".format(DEVICE['uuid'])
+
+    try:
+        response = requests.request("POST", "http://{}/messages".format(HOST), headers=headers, data=json.dumps(body), auth=(DEVICE['uuid'], DEVICE['token']))
+    except requests.ConnectionError:
+        print "Couldn't publish to EMBERS"
+        exit(1)
+
+    if response.status_code is 204:
+        print "Message sent with success"
+    else:
+        print "Something went wrong. Status Code returned: {}".format(response.status_code)
+
+main()
