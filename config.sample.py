f9ae0d709188dd03d6842c200ab0c19ae476cdcc
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-03 13:44:12 +0100

    remove historical snippets

--- config.sample.py
+++
@@ -1,9 +0,0 @@
-auth = {
-	"uuid": "7b11d04b-fefa-4add-a77c-ddd2c38ad55d",
-	"token": "907bea16d9cc7f511bcf7e4b45a3742aa22e3520"
-}
-device = {
-	"uuid": "728ba9f2-9ff8-4f0a-911d-c58a5480a011",
-	"token": "056a5fbe39e5e463c018bd4194c0a0c83379323a"
-}
-broker_address = "127.0.0.1"
