92e28378993550588b4bd04ed8718f622b4cb924
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-03 18:15:01 +0100

    tests: add subscriber-based checks on test_http_client

--- tests/test_http_client.py
+++ tests/test_http_client.py
@@ -25,15 +25,22 @@ def test_get_devices(api):
     assert Device.uuid in uuids
 
 
-def test_publish(api):
+def test_publish(api, subscriber):
     payload = {
         "publish_from_device": Device.uuid,
         "to_gateway": Gateway.uuid,
     }
     api.auth = Device.auth
-    target_uuid = Gateway.uuid
-    ret = api.publish(target_uuid, payload)
+
+    subscriber.auth = Gateway.auth
+    subscriber.subscribe(Gateway.uuid)
+
+    ret = api.publish(Gateway.uuid, payload)
     assert ret == None
+
+    msg = subscriber.get_message()
+    assert msg.payload == payload
+    assert msg.from_uuid == Device.uuid
 
 
 def test_unregister_devices(api):
