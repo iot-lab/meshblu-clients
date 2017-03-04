08bc8bf21bd336705411ef28b6642926b667d798
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-04 14:46:21 +0100

    tests: add subscriber.subscribe_device(), isolate client creation

--- tests/subscriber.py
+++ tests/subscriber.py
@@ -7,13 +7,12 @@ class OneShotSubscriber(threading.Thread):
     broker = "broker host"
     auth = ("uuid", "token")
 
+    def subscribe_device(self, device):
+        self.auth = device.auth
+        self.subscribe(device.uuid)
+
     def subscribe(self, target_uuid):
-        client = mqtt.Client()
-        client.username_pw_set(*self.auth)
-        client.on_connect = on_connect
-        client.on_message = on_message
-        client.target_uuid = target_uuid
-        client.connect(self.broker)
+        client = _client(self.broker, self.auth, target_uuid)
         self.client = client
         self.start()
 
@@ -26,6 +25,16 @@ class OneShotSubscriber(threading.Thread):
 
     def run(self):
         self.client.loop_forever()
+
+
+def _client(broker_host, auth, target_uuid):
+    client = mqtt.Client()
+    client.username_pw_set(*auth)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.target_uuid = target_uuid
+    client.connect(broker_host)
+    return client
 
 def on_connect(client, userdata, flags, rc):
     topic = client.target_uuid
