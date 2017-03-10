6fe1d8c20c44e62be2c3d455c90edc0a49d94f32
Olivier Fambon <olivier.fambon@inria.fr>
2017-03-10 17:47:42 +0100

    tests: fix runner to import pytest, print output on error

--- tests/runner.py
+++ tests/runner.py
@@ -1,11 +1,14 @@
 import subprocess
 import json
 
+import pytest
+
 
 def run(cmd, raw=False):
     cmd = cmd.split()
     try:
         ret = subprocess.check_output(cmd)
     except Exception as e:
+        print(e.output)
         pytest.fail(e)
     return ret if raw else json.loads(ret)
